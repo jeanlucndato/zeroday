658b267c99bca85f2e77225617dc83304488ff4a
jeanlucndato <jeanlucndato@gmail.com>
2024-04-22 05:55:56 -0400

    make some modif

--- inverseblog/urls.py
+++ inverseblog/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
